08bde5c6cb04744d577a7e9505721e63904b4c9d
Hardik Seju <hardik321seju@gmail.com>
2022-09-03 21:52:03 +0530

    updated base.html file

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def home():
 def result():
 
     if request.method == 'POST':
-        return redirect("home")
+        return render_template("home.html")
     
     return render_template("result.html")
 
